0150d69afa8cfd652b63061e024f7af8fdf573a2
Aima228 <bnaic2002@gmail.com>
2024-03-10 23:32:31 +0200

    fixed some bugs in front + created info part for elements from images in retail

--- js/scripts.js
+++ js/scripts.js
@@ -102,3 +102,35 @@ function startCountersWhenVisible() {
     });
 }
 startCountersWhenVisible();
+
+const swiper = new Swiper('.swiper', {
+
+    // If we need pagination
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    pagination: false,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    //   // Responsive breakpoints
+    //   breakpoints: {
+    //   // when window width is >= 320px
+    //   320: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 20
+    //   },
+    //   // when window width is >= 480px
+    //   480: {
+    //     slidesPerView: 3,
+    //     spaceBetween: 30
+    //   },
+    // }
+});
