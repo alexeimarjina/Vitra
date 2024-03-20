e089f35f76ae7261b2d1794ec021cef8d1eefedf
Aima228 <bnaic2002@gmail.com>
2024-03-20 15:13:00 +0200

    category changed into industry

---
+++ src/js/industry.js
@@ -0,0 +1,47 @@
+const categorySlider = new Swiper('.cardImgSlider', {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+const moreIdeas = new Swiper('.moreIdeas', {
+
+    // If we need pagination
+    slidesPerView: 4,
+    spaceBetween: 15,
+    pagination: false,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 320px
+        // 320: {
+        //     slidesPerView: 1,
+        //     spaceBetween: 20
+        // },
+        // when window width is >= 480px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        481: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 15
+        },
+    }
+});
